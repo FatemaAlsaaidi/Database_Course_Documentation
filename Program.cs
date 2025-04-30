d70698c02f45b4ac41064103899c0d4f1863221f
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-04-30 10:59:46 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Database_Search_and_Reporting_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
